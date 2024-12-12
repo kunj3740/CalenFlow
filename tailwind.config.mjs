e964adfffcde602e61974334a61f608cab376e04
kunj3740 <kunjdave694@gmail.com>
2024-12-12 15:05:07 +0530

    building

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+import { someFunction } from 'some-package'; 
+
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    // Add other paths here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
